5afb777298f1e4b633fcb638e61a232d8ef9e379
andre-tanaka-loft <andre.tanaka.sciensa@loft.com.br>
2020-06-17 21:14:37 -0300

    feat: route /repositories/:id implemented

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ const repositories = [];
 app.get("/repositories", (request, response) => {
   // TODO Rota que lista todos os repositórios;
 
-  return response.json(repositories)
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
@@ -23,17 +23,33 @@ app.post("/repositories", (request, response) => {
         { id: "uuid", title: 'Desafio Node.js', url: 'http://github.com/...', techs: ["Node.js", "..."], likes: 0 }; 
         Certifique-se que o ID seja um UUID, e de sempre iniciar os likes como 0.
    */
-   const {title, url, techs} = request.body
-   const repository = { id: uuid(), title, url, techs}
+  const { title, url, techs } = request.body;
+  const repository = { id: uuid(), title, url, techs };
 
-   repositories.push(repository)
+  repositories.push(repository);
 
-   return response.status(201).json(repository)
+  return response.status(201).json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
   // TODO
   /* A rota deve alterar apenas o title, a url e as techs do repositório que possua o id igual ao id presente nos parâmetros da rota; */
+  const { id } = request.params;
+  const repositoryIndex = repositories.findIndex(
+    (repository) => repository.id === id
+  );
+
+  if (repositoryIndex < 0) {
+    return response.status(400).json({ error: "Repository not found" });
+  }
+
+  const { title, url, techs } = request.body;
+
+  const repository = { id, title, url, techs };
+
+  repositories[repositoryIndex] = repository;
+
+  return response.json(repository);
 });
 
 app.delete("/repositories/:id", (request, response) => {
